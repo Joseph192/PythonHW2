117f0be85ec12d8e80939b25ded8ac286badf225
Joseph192 <61397433+Joseph192@users.noreply.github.com>
2021-04-28 21:09:25 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+class Employees:
+    raise_amount = 2000
+    def __init__(self, first_name, last_name, salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+        self.current_raise_amount = Employees.raise_amount
+
+    def add_salary(self, salary):
+        self.salary = self.salary + self.current_raise_amount
+
+    def get_full_name(self, first_name, last_name):
+        return f"{self.first_name} {self.last_name}"
+
+    @classmethod
+    def increase_raise_amount(cls, amount):
+        cls.raise_amount = cls.raise_amount + amount
+
